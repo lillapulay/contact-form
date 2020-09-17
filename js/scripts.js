b5be1fda4ab42a1d805c7e1d7add9d30c73b7492
lillapulay <59831508+lillapulay@users.noreply.github.com>
2020-09-17 14:37:56 +0200

    Moved preventDefault to each validator
    So the submit action is only prevented if any of the fields is invalid, but passes if they are all filled correctly

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
 console.log('It works')
 
 $(document).ready(function () { // Ready: because we load the file in the head tag
-  $('.submit').click(function (event) { // Targets the button and makes it listen to a click event
-    event.preventDefault() // So the form doesn't automatically post as the submit button would suggest, but we want to wait for validation first
+  $('.submit').click(function (event) { // Targets the button and makes it listen to a click event    
     console.log('Clicked button')
 
     //Defining variables (fields)
@@ -15,18 +14,21 @@ $(document).ready(function () { // Ready: because we load the file in the head t
     if (email.length > 5 && email.includes('@') && email.includes('.')) { // Email has to be 5+ chars, include @ and .
       statusElm.append('<div>Email is valid</div>') // Appends the messages to the status element
     } else {
+      event.preventDefault()
       statusElm.append('<div>Email is not valid</div>')
     }
 
     if (subject.length >= 2) { // Subject has to be 2 characters or more
       statusElm.append('<div>Subject is valid</div>') // Appends the messages to the status element
     } else {
+      event.preventDefault()
       statusElm.append('<div>Subject is not valid</div>')
     }
 
     if (message.length > 10) { // Message has to be 10+ characters
       statusElm.append('<div>Message is valid</div>') // Appends the messages to the status element
     } else {
+      event.preventDefault()
       statusElm.append('<div>Message is not valid</div>')
     }
   })
