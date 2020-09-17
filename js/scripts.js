30f1af25f5b95cb21f111dcbb8da4f1ff4a5ca72
lillapulay <59831508+lillapulay@users.noreply.github.com>
2020-09-17 14:12:08 +0200

    Added email requirements

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,16 @@ $(document).ready(function () { // Ready: because we load the file in the head t
     event.preventDefault() // So the form doesn't automatically post as the submit button would suggest, but we want to wait for validation first
     console.log('Clicked button')
 
+    //Defining variables (fields)
     var email = $('.email').val()
     var subject = $('.subject').val()
     var message = $('.message').val()
+
+
+    if (email.length > 5 && email.includes('@') && email.includes('.')) { // Email has to be 5+ chars, include @ and .
+      console.log('Email is valid')
+    } else {
+      console.log('Email is not valid')
+    }
   })
 })
