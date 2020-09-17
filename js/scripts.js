96858aec151e8e48ec50b7ca0e2214b309022f2c
lillapulay <59831508+lillapulay@users.noreply.github.com>
2020-09-17 14:07:27 +0200

    Added event listener to submit button

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
 console.log('It works')
+
+$(document).ready(function () { // Ready: because we load the file in the head tag
+  $('.submit').click(function (event) { // Targets the button and makes it listen to a click event
+    event.preventDefault() // So the form doesn't automatically post as the submit button would suggest, but we want to wait for validation first
+    console.log('Clicked button')
+  })
+})
